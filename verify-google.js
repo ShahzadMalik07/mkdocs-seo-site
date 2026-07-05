9e19aef1edc4d411fac39af9654e6b0672c26e19
ShahzadMalik07 <shahzadmalik425@gmail.com>
2026-07-06 01:28:37 +0530

    meta tag added

---
+++ verify-google.js
@@ -0,0 +1,4 @@
+var meta = document.createElement('meta');
+meta.name = "google-site-verification";
+meta.content = "anbIc3lVBfsfpYwUbU1z1xwf_ccuYPrf_1TbIrTa4go";
+document.getElementsByTagName('head')[0].appendChild(meta);
