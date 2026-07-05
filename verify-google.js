40d607d123b749055d2aa826ceb3f19ecb80f452
ShahzadMalik07 <shahzadmalik425@gmail.com>
2026-07-06 01:52:07 +0530

    changes

--- verify-google.js
+++
@@ -1,4 +0,0 @@
-var meta = document.createElement('meta');
-meta.name = "google-site-verification";
-meta.content = "anbIc3lVBfsfpYwUbU1z1xwf_ccuYPrf_1TbIrTa4go";
-document.getElementsByTagName('head')[0].appendChild(meta);
